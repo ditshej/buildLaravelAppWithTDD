2d6383c0dba7b570f2f69e6ced3e989c3f9e94ac
Raphi W <ditshej@gmail.com>
2021-05-22 11:38:15 +0200

    ep 2

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,18 +9,16 @@ class ProjectsController extends Controller
     public function index()
     {
         $projects = Project::all();
-
         return view('projects.index', compact('projects'));
     }
 
     public function store()
     {
-        // validate
-
-        // persist
-        Project::create(request(['title', 'description']));
-
-        // redirect
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        Project::create($attributes);
         return redirect('/projects');
     }
 }
