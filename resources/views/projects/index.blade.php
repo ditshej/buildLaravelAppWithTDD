a74ae13a452491d6694618767ad6e326bdad2b95
Raphi W <ditshej@gmail.com>
2021-05-24 15:50:44 +0200

    ep 9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,11 +6,16 @@
         <a href="/projects/create">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex flex-wrap">
         @forelse($projects as $project)
-            <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+            <div class="bg-white mr-4 mb-4 rounded p-5 shadow w-1/3" style="height: 200px;">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+                <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+            </div>
         @empty
             <li>No projects yet.🏄‍</li>
         @endforelse
-    </ul>
+    </div>
+
 @endsection
